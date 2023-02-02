60465758b9d7c2f9cb779e7616bc267654dc47c3
syuya2036 <syuya2036@gmail.com>
2023-02-02 10:10:12 +0900

    update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Mlink
+
+This is a linebot that converts shared links between music sites into each other.
+
+## Supported services
+
+- Youtube Music
+- Apple Music
+- Amazon Music
+- Spotify
+- (LINE Music)
--- main.go
+++ main.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"log"
 	"net/http"
-	"os"
-	"github.com/syuya2036/Mlink/anylink"
-	"github.com/line/line-bot-sdk-go/v7/linebot"
+	"io"
+	"regexp"
+	"encoding/json"
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
+	const ChannelSecret = "e8955790558134481225f16a34291401"
+	const AccessToken = "e/ilqffzd3mP20M5NrWcaIQK54xIYkybduJfzGB2Fd9ZML/ODeELFmfXlQgr9TnGb4JikPgYDXOXDWDgXn6LWO2OcO3z/dfVXWJ5U+BjwFyCvf7Lcjz0WoUn3kFAAhmhPtEhxizBjeHGTKGofvfFVQdB04t89/1O/w1cDnyilFU="
 	bot, err := linebot.New(
-		os.Getenv("CHANNEL_SECRET"),
-		os.Getenv("CHANNEL_TOKEN"),
+		ChannelSecret, // Channel Secret 
+		AccessToken, // アクセストークン（ロングターム）
 	)
 	if err != nil {
 		log.Fatal(err)
@@ -33,12 +37,8 @@ func main() {
 			if event.Type == linebot.EventTypeMessage {
 				switch message := event.Message.(type) {
 				case *linebot.TextMessage:
-					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
-						log.Print(err)
-					}
-				case *linebot.StickerMessage:
-					replyMessage := fmt.Sprintf(
-						"sticker id is %s, stickerResourceType is %s", message.StickerID, message.StickerResourceType)
+					replyMessage := create_message(message.Text)
+
 					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do(); err != nil {
 						log.Print(err)
 					}
@@ -51,4 +51,112 @@ func main() {
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+
+// 音楽サイトの各リンクとタイトル・アーティスト
+type Minfo struct {
+	Url string
+	Title string
+	ArtistName string
+	Amazon string
+	Apple string
+	Youtube string
+	Spotify string
+	Line string
+}
+
+
+func NewMinfo(url string) *Minfo {
+	self := new(Minfo)
+	self.Url = url
+
+	return self
+}
+
+
+// jsonを解析して各種情報を取得
+func (self *Minfo) readMap(songUrlMap map[string]interface {}) {
+	links_by_platform := songUrlMap["linksByPlatform"].(map[string]interface {})
+
+	for key, val := range links_by_platform {
+		if key == "amazonMusic" {
+			self.Amazon = fmt.Sprintf("%v", val.(map[string]interface {})["url"])
+		} else if key == "appleMusic" {
+			self.Apple = fmt.Sprintf("%v", val.(map[string]interface {})["url"])
+		} else if key == "youtubeMusic" {
+			self.Youtube = fmt.Sprintf("%v", val.(map[string]interface {})["url"])
+		} else if key == "spotify" {
+			self.Spotify = fmt.Sprintf("%v", val.(map[string]interface {})["url"])
+		}
+	}
+
+	entities_by_unique_id := songUrlMap["entitiesByUniqueId"].(map[string]interface {})
+
+	for _, val := range entities_by_unique_id {
+		title := fmt.Sprintf("%v", val.(map[string]interface {})["title"])
+		artistname := fmt.Sprintf("%v", val.(map[string]interface {})["artistName"])
+
+		self.Title = strings.Replace(title, " ", "", -1)
+		self.ArtistName = strings.Replace(artistname, " ", "", -1)
+
+		self.Line = "https://music.line.me/webapp/search?query=" + self.Title + "%20" + self.ArtistName
+
+		break
+	}
+}
+
+func (self *Minfo) GetMusicUrls() error {
+	// APIを叩く
+	resp, err := http.Get(self.Url)
+
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		err := fmt.Errorf("Status code %d", resp.StatusCode)
+		log.Print(err)
+		return err
+	}
+
+	body, _ := io.ReadAll(resp.Body)
+
+	// jsonフォーマットをmapに変換
+	var songUrlMap map[string]interface{}
+	json.Unmarshal(body, &songUrlMap)
+
+	self.readMap(songUrlMap)
+
+	return nil
+}
+
+
+func is_url(req_str string) bool {
+	re := regexp.MustCompile("^https.*")
+    return re.MatchString(req_str)
+}
+
+func create_message(requested_message string) string {
+	if is_url(requested_message) {
+		request_url := "https://api.song.link/v1-alpha.1/links?url=" + requested_message + "&userCountry=JP"
+	
+		minfo := NewMinfo(request_url)
+		err := minfo.GetMusicUrls()
+
+		if err != nil{
+			replyText := "リンクは存在しないよ！"
+			return replyText
+		}
+		replyText := "Artist: " + minfo.ArtistName  + "\nTitle: " + minfo.Title + 
+					"\nAmazon:\n" + minfo.Amazon + "\nApple:\n" + minfo.Apple + 
+					"\nSpotify:\n"+ minfo.Spotify + "\nYoutube:\n" + minfo.Youtube + "\nLine:\n" + minfo.Line
+
+
+		return replyText
+	} else {
+		replyText := "リンクを送ってね！"
+		return 
 }
