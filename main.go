b728ff45fcd5d7de20bf99bf9c89308d4506d465
shuyagoto <93831851+syuya2036@users.noreply.github.com>
2023-02-09 20:45:01 +0900

    add comment

--- main.go
+++ main.go
@@ -47,8 +47,7 @@ func main() {
 			}
 		}
 	})
-	// This is just sample code.
-	// For actual use, you must support HTTPS by using `ListenAndServeTLS`, a reverse proxy or something else.
+	
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
